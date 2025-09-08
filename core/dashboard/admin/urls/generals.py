b10ff471f5bbf461d58c19c14786eafacae73ba6
mahdiqpzm@gmail.com <azxmahdi22@gmail.com>
2025-09-08 20:06:43 +0330

    fix bug

--- core/dashboard/admin/urls/generals.py
+++ core/dashboard/admin/urls/generals.py
@@ -4,14 +4,4 @@ from .. import views
 
 urlpatterns = [
     path("home/", views.HomeView.as_view(), name="home"),
-    path(
-        "security/edit/",
-        views.AdminSecurityEditView.as_view(),
-        name="security-edit",
-    ),
-    path(
-        "profile/edit/",
-        views.AdminProfileEditView.as_view(),
-        name="profile-edit",
-    ),
 ]
